e188f9ff1325f867a3db0c88edfef327c0beb4b1
Cristi Duma <cristiduma@cristi-mac.local>
2008-09-19 15:07:48 +0300

    Added tag descriptions

--- lib/related_content/tags.rb
+++ lib/related_content/tags.rb
@@ -6,17 +6,35 @@ module RelatedContent::Tags
     tag.locals.related_pages = page.related_pages
     tag.expand
   end
-  
+
+  desc %{
+    Renders the first related page. 
+    
+    *Usage:*
+    <pre><code><r:related:first>...</r:related:first></code></pre>
+  }
   tag 'related:first' do |tag|
     tag.locals.page = tag.locals.related_pages.to_a.first
     tag.expand
   end
-
+  
+  desc %{
+    Renders the last related page. 
+    
+    *Usage:*
+    <pre><code><r:related:last>...</r:related:last></code></pre>
+  }
   tag 'related:last' do |tag|
     tag.locals.page = tag.locals.related_pages.to_a.last
     tag.expand
   end
-  
+
+  desc %{
+    Cycles through each of the related pages and renders them.
+    
+    *Usage:*
+    <pre><code><r:related:each>...</r:related:each></code></pre>
+  }
   tag 'related:each' do |tag|
     returning String.new do |output|
       tag.locals.related_pages.each do |page|
