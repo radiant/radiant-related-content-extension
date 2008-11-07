b635a19dea9c1e20f32479bf9f94adbf170d23b6
Cristi Duma <cristi.duma@aissac.ro>
2008-11-07 13:18:54 +0200

    spec for the sort controller

--- lib/related_content/related_content_sort.rb
+++ lib/related_content/related_content_sort.rb
@@ -2,9 +2,8 @@ module RelatedContent::RelatedContentSort
 
   def sort 
     @page = Page.find(params[:id])
-    indices = @page.outgoing_relations.map(&:to_id)  
-    @page.outgoing_relations.each_with_index do |relation, index|
-      relation.position = params['related_content_container'].index(indices[index].to_s) + 1 
+    @page.outgoing_relations.each do |relation|
+      relation.position = params['related_content_container'].index(relation.to_id.to_s) + 1 
       relation.save 
     end 
     render :nothing => true 
---
+++ spec/controllers/related_content_sort_spec.rb
@@ -0,0 +1,28 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Admin::PageController do
+  scenario :users, :related_content
+  
+  before do
+    login_as :developer
+    @page = pages(:first)
+    
+    @first = @page.outgoing_relations[0]
+    @second = @page.outgoing_relations[1]
+    
+    Page.stub!(:find).and_return(@page)
+  end
+  
+  def do_sort
+    post :sort, :id => page_id(:first), :related_content_container => [@second.to_id.to_s, @first.to_id.to_s]
+  end
+  
+  it "assigns new positions" do
+    @first.should_receive(:position=).with(2)
+    @first.should_receive(:save)
+    @second.should_receive(:position=).with(1)
+    @second.should_receive(:save)
+    do_sort
+  end
+
+end
